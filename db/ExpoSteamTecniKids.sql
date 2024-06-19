46811b48fc5aaee540d987b35affbe573adcc00d
euboaz <euboaz@hotmail.com>
2024-06-18 22:02:38 -0600

    Base de Datos terminada

--- db/ExpoSteamTecniKids.sql
+++ db/ExpoSteamTecniKids.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 16-06-2024 a las 04:16:23
+-- Tiempo de generación: 19-06-2024 a las 06:01:48
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -24,12 +24,76 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `calificacion`
+--
+
+CREATE TABLE `calificacion` (
+  `id_calificacion` int(11) NOT NULL,
+  `id_evaluacion` int(11) NOT NULL,
+  `id_penalizacion` int(11) NOT NULL,
+  `calificacion_final` double NOT NULL,
+  `id_desempate` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categoria`
+--
+
+CREATE TABLE `categoria` (
+  `id_categoria` int(11) NOT NULL,
+  `nombre_categoria` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `desempate`
+--
+
+CREATE TABLE `desempate` (
+  `id_desempate` int(11) NOT NULL,
+  `criterio_desempate` varchar(255) NOT NULL,
+  `puntaje` decimal(7,0) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `equipo`
+--
+
+CREATE TABLE `equipo` (
+  `id_equipo` int(11) NOT NULL,
+  `juez_nickname` varchar(30) NOT NULL,
+  `numero_equipo` int(11) NOT NULL,
+  `nombre_institucion` varchar(100) NOT NULL,
+  `id_categoria` int(11) NOT NULL,
+  `id_calificacion` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `evaluacion`
+--
+
+CREATE TABLE `evaluacion` (
+  `id_evaluacion` int(11) NOT NULL,
+  `criterio_evaluacion` varchar(255) NOT NULL,
+  `puntaje` decimal(7,0) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `juez`
 --
 
 CREATE TABLE `juez` (
-  `juez_nickname` varchar(20) NOT NULL,
-  `juez_pass` varchar(20) NOT NULL
+  `juez_nickname` varchar(30) NOT NULL,
+  `juez_pass` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -40,15 +104,129 @@ INSERT INTO `juez` (`juez_nickname`, `juez_pass`) VALUES
 ('ebogantes', '123456'),
 ('vsalas', '123456');
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `penalizacion`
+--
+
+CREATE TABLE `penalizacion` (
+  `id_penalizacion` int(11) NOT NULL,
+  `criterio_penalizacion` varchar(255) NOT NULL,
+  `puntaje` decimal(7,0) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `calificacion`
+--
+ALTER TABLE `calificacion`
+  ADD PRIMARY KEY (`id_calificacion`),
+  ADD KEY `FK_id_evaluacion` (`id_evaluacion`),
+  ADD KEY `FK_id_penalizacion` (`id_penalizacion`),
+  ADD KEY `FK_id_desempate` (`id_desempate`);
+
+--
+-- Indices de la tabla `categoria`
+--
+ALTER TABLE `categoria`
+  ADD PRIMARY KEY (`id_categoria`);
+
+--
+-- Indices de la tabla `desempate`
+--
+ALTER TABLE `desempate`
+  ADD PRIMARY KEY (`id_desempate`);
+
+--
+-- Indices de la tabla `equipo`
+--
+ALTER TABLE `equipo`
+  ADD PRIMARY KEY (`id_equipo`),
+  ADD KEY `FK_juez_asignado` (`juez_nickname`),
+  ADD KEY `FK_id_calificacion` (`id_calificacion`),
+  ADD KEY `FK_id_categoria` (`id_categoria`);
+
+--
+-- Indices de la tabla `evaluacion`
+--
+ALTER TABLE `evaluacion`
+  ADD PRIMARY KEY (`id_evaluacion`);
 
 --
 -- Indices de la tabla `juez`
 --
 ALTER TABLE `juez`
   ADD PRIMARY KEY (`juez_nickname`);
+
+--
+-- Indices de la tabla `penalizacion`
+--
+ALTER TABLE `penalizacion`
+  ADD PRIMARY KEY (`id_penalizacion`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `calificacion`
+--
+ALTER TABLE `calificacion`
+  MODIFY `id_calificacion` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `categoria`
+--
+ALTER TABLE `categoria`
+  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `desempate`
+--
+ALTER TABLE `desempate`
+  MODIFY `id_desempate` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `equipo`
+--
+ALTER TABLE `equipo`
+  MODIFY `id_equipo` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `evaluacion`
+--
+ALTER TABLE `evaluacion`
+  MODIFY `id_evaluacion` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `penalizacion`
+--
+ALTER TABLE `penalizacion`
+  MODIFY `id_penalizacion` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `calificacion`
+--
+ALTER TABLE `calificacion`
+  ADD CONSTRAINT `FK_id_desempate` FOREIGN KEY (`id_desempate`) REFERENCES `desempate` (`id_desempate`),
+  ADD CONSTRAINT `FK_id_evaluacion` FOREIGN KEY (`id_evaluacion`) REFERENCES `evaluacion` (`id_evaluacion`),
+  ADD CONSTRAINT `FK_id_penalizacion` FOREIGN KEY (`id_penalizacion`) REFERENCES `penalizacion` (`id_penalizacion`);
+
+--
+-- Filtros para la tabla `equipo`
+--
+ALTER TABLE `equipo`
+  ADD CONSTRAINT `FK_id_calificacion` FOREIGN KEY (`id_calificacion`) REFERENCES `calificacion` (`id_calificacion`),
+  ADD CONSTRAINT `FK_id_categoria` FOREIGN KEY (`id_categoria`) REFERENCES `categoria` (`id_Categoria`),
+  ADD CONSTRAINT `FK_juez_asignado` FOREIGN KEY (`juez_nickname`) REFERENCES `juez` (`juez_nickname`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
